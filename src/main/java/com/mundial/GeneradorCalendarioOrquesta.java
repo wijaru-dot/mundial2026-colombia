24adc9efd05c3abcd3b2010deb15fb7f14a10573
William <williamjavier.ruizgonzalez@gmail.com>
2026-04-11 17:31:02 -0400

    fix: mover clase a package com.mundial

--- src/main/java/com/mundial/GeneradorCalendarioOrquesta.java
+++ src/main/java/com/mundial/GeneradorCalendarioOrquesta.java
@@ -1,3 +1,5 @@
+package com.mundial;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
